8749fa366afe2ee2c811c47eea0535bc96c4387f
Rafael Carrera Rodriguez <rafael.carrera-rodriguez@lirmm.fr>
2024-10-27 18:04:32 +0100

    Add new changes to my conf

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -156,6 +156,9 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Remove the ~ and folding stuff
+vim.wo.fillchars = 'eob: '
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -616,7 +619,7 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        --
+        texlab = {},
 
         lua_ls = {
           -- cmd = {...},
@@ -829,13 +832,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'sainnhe/edge',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'edge'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -856,7 +859,7 @@ require('lazy').setup({
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      -- Add/delete/eplace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
@@ -893,6 +896,7 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
@@ -918,18 +922,18 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,128 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+--
+local plugins = {
+  {
+    'yamatsum/nvim-cursorline',
+    opts = {},
+  },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {
+      'kevinhwang91/promise-async',
+      'neoclide/coc.nvim',
+    },
+    opts = function()
+      vim.o.foldcolumn = '0' -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+    end,
+  },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_view_general_viewer = 'okular'
+      vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
+    end,
+  },
+  -- {
+  --   'akinsho/bufferline.nvim',
+  --   dependencies = 'nvim-tree/nvim-web-devicons',
+  --   init = function()
+  --     vim.opt.termguicolors = true
+  --     vim.keymap.set('n', '<leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>')
+  --     vim.keymap.set('n', '<leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>')
+  --     vim.keymap.set('n', '<leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>')
+  --     vim.keymap.set('n', '<leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>')
+  --     vim.keymap.set('n', '<leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>')
+  --     vim.keymap.set('n', '<leader>6', '<Cmd>BufferLineGoToBuffer 6<CR>')
+  --     vim.keymap.set('n', '<leader>7', '<Cmd>BufferLineGoToBuffer 7<CR>')
+  --     vim.keymap.set('n', '<leader>8', '<Cmd>BufferLineGoToBuffer 8<CR>')
+  --     vim.keymap.set('n', '<leader>9', '<Cmd>BufferLineGoToBuffer 9<CR>')
+  --     vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>')
+  --     vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>')
+  --   end,
+  --   opts = {
+  --     options = {
+  --       separator_style = 'slant',
+  --       offsets = {
+  --         filetype = 'neo-tree',
+  --         text = 'File Explorer',
+  --         highlight = 'Directory',
+  --         text_align = 'left',
+  --       },
+  --     },
+  --   },
+  -- },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    init = function()
+      vim.keymap.set('n', '<A-t>', '<Cmd>ToggleTerm<CR>')
+    end,
+    opts = {},
+  },
+  {
+    'github/copilot.vim',
+  },
+  {
+    'goolord/alpha-nvim',
+    dependencies = 'kyazdani42/nvim-web-devicons',
+    init = function()
+      vim.keymap.set('n', '<A-a>', '<Cmd>Alpha<CR>')
+    end,
+    config = function()
+      require('alpha').setup(require('alpha.themes.startify').opts)
+    end,
+  },
+  {
+    'hadronized/hop.nvim',
+    init = function()
+      vim.keymap.set('n', '<A-h>', '<Cmd>HopWord<CR>')
+    end,
+    opts = {},
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    init = function()
+      vim.keymap.set('n', '<Tab>', '<Cmd>bnext<CR>')
+      vim.keymap.set('n', '<S-Tab>', '<Cmd>bprev<CR>')
+    end,
+    opts = {
+      options = {
+        theme = 'auto',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          {
+            'branch',
+            separator = { right = '' },
+          },
+          {
+            'diff',
+            separator = { right = '' },
+          },
+          'buffers',
+        },
+        lualine_c = { '' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+    },
+  },
+}
+
+return plugins
