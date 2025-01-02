083427c848770d130458b0d79c8e7551152c4779
Rafael Carrera Rodriguez <rafael.carrera-rodriguez@lirmm.fr>
2025-01-02 23:20:21 +0100

    Add files

--- init.lua
+++ init.lua
@@ -620,6 +620,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         texlab = {},
+        ltex = {},
 
         lua_ls = {
           -- cmd = {...},
